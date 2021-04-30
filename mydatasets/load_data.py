bbbefd80ebee640212be1fef796b2dcb237c536b
mahartmann <hartmann@di.ku.dk>
2021-04-30 13:36:34 +0200

    added task specs

--- mydatasets/load_data.py
+++ mydatasets/load_data.py
@@ -5,11 +5,11 @@ import os
 def get_data(task, split, config, tokenizer):
     if task.dataset_type == 'relclassification':
         data = RelClassificationDataset(read_relation_classification_examples(
-            os.path.join(config.get('Files', 'preproc_data'), '{}_{}.jsonl'.format(task.dataset, split))),
+            os.path.join(config.get('Files', 'preproc_data'), '{}_{}.json'.format(task.dataset, split))),
             tokenizer=tokenizer, max_seq_len=task.max_seq_len)
     elif task.dataset_type == 'negscope':
         data = NegScopeDataset(
             read_examples(
-                os.path.join(config.get('Files', 'preproc_data'), '{}_{}.jsonl'.format(task.dataset, split))),
+                os.path.join(config.get('Files', 'preproc_data'), '{}_{}.json'.format(task.dataset, split))),
             tokenizer=tokenizer, max_seq_len=task.max_seq_len, split_seqs=task.split_seqs)
     return data
